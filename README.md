b5dfb2ffa01e927b2faa7c2f5aeaa0b29242c2c0
Souvik Dutta1 <souvik_dutta1@epam.com>
2022-10-20 12:28:50 +0530

    added bare minimum code for the requirement of the assignment 4

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ Swagger Documentation: https://487s17x3x7.execute-api.us-east-1.amazonaws.com/sw
 
 getProductsList endpoint: https://487s17x3x7.execute-api.us-east-1.amazonaws.com/products
 getProductsById endpoint: https://487s17x3x7.execute-api.us-east-1.amazonaws.com/products/7567ec4b-b10c-48c5-9345-fc73348a80a1
+
+create Product endpoint: https://8dt3chtur0.execute-api.ap-south-1.amazonaws.com/products
+with the request body:
+{
+"title":"Product11",
+"description": "Nice product",
+"price": 10,
+"count": 10
+}
--- product-service/getProductsList.js
+++ product-service/getProductsList.js
@@ -1,17 +1,44 @@
-"use strict";
-const data = require("./assets/data.json");
+const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
 
-module.exports.getProductsList = async (event) => {
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const paramsProducts = {
+  TableName: process.env.PRODUCTS_TABLE,
+};
+
+module.exports.getProductsList = async (event, context, callback) => {
   try {
+    const products = await dynamoDb.scan(paramsProducts).promise();
+
+    const combinedResult = await Promise.all(
+      products.Items.map(async (product) => {
+        const paramStocks = {
+          TableName: process.env.STOCKS_TABLE,
+          Key: {
+            product_id: product.id,
+          },
+        };
+        try {
+          const productCount = await dynamoDb.get(paramStocks).promise();
+          return {
+            ...product,
+            count: productCount.Item.count,
+          };
+        } catch (err) {
+          throwError(err);
+          return;
+        }
+      })
+    );
+
     return {
       statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
-      },
-      body: JSON.stringify(data),
+      body: JSON.stringify(combinedResult),
     };
-  } catch (e) {
-    console.log("error", e);
+  } catch (error) {
+    return {
+      statusCode: error.statusCode || 501,
+      headers: { "Content-Type": "text/plain" },
+      body: "Couldn't fetch the products",
+    };
   }
 };
